1dadd9a5ab184fc789e88e2fa8104cf8e00dcdbe
Sunny <sunnyinvadeher@gmail.com>
2016-08-04 20:29:56 +0800

    加入一部分的FB認證

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ var express = require('express');
 var fs = require('fs');
 var passport = require('passport')
   , FacebookStrategy = require('passport-facebook').Strategy;
+var app = express();
 
+passport.use(new FacebookStrategy({
+    clientID: 1742952685970599,
+    clientSecret: 2c03c040c02e08e665b9c4d506df66f8,
+    callbackURL: "http://localhost:8088/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
 
-  
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
