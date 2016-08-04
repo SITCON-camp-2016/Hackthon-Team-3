27f1e9b4d58c59c58adabc205cb0b71f1d1b39c6
Sunny <sunnyinvadeher@gmail.com>
2016-08-04 23:05:03 +0800

    一坨能動的大便(可以用FB取得ID了)

--- main.js
+++ main.js
@@ -2,20 +2,32 @@ var express = require('express');
 var fs = require('fs');
 var passport = require('passport')
   , FacebookStrategy = require('passport-facebook').Strategy;
+var cookieParser = require('cookie-parser') 
 var app = express();
+var ID = "0";
+
+app.use(cookieParser())
+app.use('/static',express.static(__dirname));
+
 
 passport.use(new FacebookStrategy({
     clientID: 1742952685970599,
-    clientSecret: 2c03c040c02e08e665b9c4d506df66f8,
-    callbackURL: "http://localhost:8088/auth/facebook/callback"
+    clientSecret: "2c03c040c02e08e665b9c4d506df66f8",
+    callbackURL: "http://localhost:8088/auth/facebook/callback",
+    passReqToCallBack: true
   },
   function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate(..., function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
+  	// console.log(JSON.stringify(profile));
+  	ID = profile.id;
+  	console.log(ID);
+   //  User.findOrCreate(function(err, user) {
+	  // if (err) { return done(err); }
+   //    done(null, user);
+   //  });
+	done(null);
   }
 ));
+
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
@@ -28,4 +40,17 @@ app.get('/auth/facebook', passport.authenticate('facebook'));
 // authentication has failed.
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/login' }));
+                                      failureRedirect: '/' }));
+
+app.get('/', function(req, res) {
+	if(ID!="0"){
+		console.log("Login!");
+		res.send(fs.readFileSync('./index2.html', 'UTF-8'));
+	}
+	else{
+		console.log("not Login!");
+		res.send(fs.readFileSync('./index.html', 'UTF-8'));
+	}
+});
+
+app.listen("8088");
