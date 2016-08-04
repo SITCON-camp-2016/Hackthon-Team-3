549e0c5078ef8937177a07a215314bb76eb06dc8
Sunny <sunnyinvadeher@gmail.com>
2016-08-04 23:08:50 +0800

    前端一個大改

--- text.js
+++ text.js
@@ -1,22 +1,11 @@
-$('.pro').validate({
-		rules:{
-			se: {
-				required: true,
-				minlength: 10;
-			}
-		}
-	})
-
-$('.pro').on('submit', function(){
-	
+$('#pro').on('submit', function(){
 	$.ajax({
   	url: 'https://httpbin.org/get',
-    data: $('.pro').serialize(),
+    data: $('#pro').serialize(),
     type: 'GET'
   })
   .done(function(data){
   	$('div.a').html(JSON.stringify(data.args))
-	
   })
 	return false;
 })
